5b2ab046fc1077bfe1c370dac6561c5edd12e25b
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2025-12-20 01:05:38 +0530

    implement simple interest calculation program

---
+++ lec-01/simpleInterest.cpp
@@ -0,0 +1,18 @@
+//calculate simple interest from principal, rate and time
+#include <iostream>
+using namespace std;
+
+int main() {
+    float principal, rate, time, simpleInterest;
+
+    cout << "Enter principal amount: ";
+    cin >> principal;
+    cout << "enter rate of interest: ";
+    cin >> rate;
+    cout << "Enter time in years: ";
+    cin >> time;
+
+    simpleInterest = (principal * rate * time) / 100;
+    cout << "Simple Interest is: " << simpleInterest << endl;
+    return 0;
+}
