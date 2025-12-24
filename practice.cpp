5a9237530d8ed990213d09caadd94407fb4b4a4b
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2025-12-25 03:18:37 +0530

    Refactor star pattern generation in practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -12,20 +12,35 @@ int main (){
 
     for (int i = 0; i < n ; i++){
         //space
-        for (int j = 0; j < n-i-1; j++){
-            cout <<" "; 
+       for (int j = n-i-1; j > 0; j--){
+        cout << " ";
+       }
+       cout << "*";
+       if (i != 0){
+        for (int j = 0; j < 2*i-1; j++){
+            cout <<" ";
         }
-        int num = 1;
-        for (int j = 0; j < i+1; j++){
-            cout << num;
-            num++;
-        }
-        if (i !=0 ){
-            for (int j = i; j > 0; j--){
-                cout << j;
-            }
-        }
+        cout << "*";
+       }
         cout <<"\n";
+    }
+    for (int i = 0; i < n-1; i++){
+
+        for (int j = 0; j < i+1; j++){
+            cout <<" ";
+        }
+        cout <<"*";
+
+        if (i != n-2){
+            
+            for (int j = 2*(n-2-i)-1; j > 0; j--){
+                cout <<" ";
+            }
+            cout <<"*";   
+
+        }
+        cout << "\n";
+
     }
     return 0;
 }
