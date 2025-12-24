a1a62f021210fe8f3de73b3b631cc27f2f7ebd4d
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2025-12-25 03:18:27 +0530

    Refactor hollow diamond pattern code

--- lec-04/hollowDiamondPattern.cpp
+++ lec-04/hollowDiamondPattern.cpp
@@ -1,37 +1,46 @@
-// hollow diamond pattern
+// practice code (revision)
 
 #include <iostream>
 using namespace std;
 
 int main (){
     int n;
-    cout << "Enter N number: ";
+    //  num = 1;
+
+    cout <<"Enter N number: ";
     cin >> n;
 
-    for (int i = 0; i < n; i++){  
+    for (int i = 0; i < n ; i++){
         //space
-        for (int j = n-i-1; j > 0; j--){
+       for (int j = n-i-1; j > 0; j--){
+        cout << " ";
+       }
+       cout << "*";
+       if (i != 0){
+        for (int j = 0; j < 2*i-1; j++){
             cout <<" ";
         }
-        cout <<"*";
-
-        if (i != 0){
-            for (int j = 0; j < 2*i-1; j++){
-                cout << " ";
-            }
-            cout <<"*";
-
-        }
+        cout << "*";
+       }
         cout <<"\n";
-    
     }
     for (int i = 0; i < n-1; i++){
+
         for (int j = 0; j < i+1; j++){
             cout <<" ";
         }
         cout <<"*";
-        cout <<"\n";
+
+        if (i != n-2){
+            
+            for (int j = 2*(n-2-i)-1; j > 0; j--){
+                cout <<" ";
+            }
+            cout <<"*";   
+
+        }
+        cout << "\n";
+
     }
     return 0;
-
 }
