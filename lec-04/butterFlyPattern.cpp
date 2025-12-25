dfb9f8f6050f0cb0d1b58859501b53b2c2ad295e
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2025-12-25 13:55:25 +0530

    butterfly pattern generation and update practice.cpp

--- lec-04/butterFlyPattern.cpp
+++ lec-04/butterFlyPattern.cpp
@@ -1,17 +1,40 @@
 // butterfly pattern
+
 #include <iostream>
 using namespace std;
 
 int main (){
     int n;
+
     cout <<"Enter N number: ";
     cin >> n;
 
     for (int i = 0; i < n; i++){
-        for (int j = 0; j < i; j++){
-            cout << "* ";
+        for (int j = 0; j < i+1; j++){
+            cout <<"*";
+        }
+        if (i != n){
+            for (int j = 0; j < (2*(n-i)-2); j++){
+                cout <<" ";
+            }
+        }   
+        for (int j = 0; j < i+1; j++){
+            cout <<"*";
+        }
+        cout<<"\n";
+    }
+    for (int i = n-1; i >= 0; i--){
+        for (int j = i; j >= 0; j--){
+            cout <<"*";
+        }
+        for (int j = 0; j < (2*(n-i)-2); j++){
+            cout <<" ";
+        }
+        for (int j = i; j >= 0; j--){
+            cout <<"*";
         }
         cout <<"\n";
     }
+    
     return 0;
 }
--- practice.cpp
+++ practice.cpp
@@ -10,37 +10,33 @@ int main (){
     cout <<"Enter N number: ";
     cin >> n;
 
-    for (int i = 0; i < n ; i++){
-        //space
-       for (int j = n-i-1; j > 0; j--){
-        cout << " ";
-       }
-       cout << "*";
-       if (i != 0){
-        for (int j = 0; j < 2*i-1; j++){
-            cout <<" ";
-        }
-        cout << "*";
-       }
-        cout <<"\n";
-    }
-    for (int i = 0; i < n-1; i++){
-
+    for (int i = 0; i < n; i++){
         for (int j = 0; j < i+1; j++){
-            cout <<" ";
+            cout <<"*";
         }
-        cout <<"*";
-
-        if (i != n-2){
-            
-            for (int j = 2*(n-2-i)-1; j > 0; j--){
+        if (i != n){
+            for (int j = 0; j < (2*(n-i)-2); j++){
                 cout <<" ";
             }
-            cout <<"*";   
-
+        }   
+        for (int j = 0; j < i+1; j++){
+            cout <<"*";
         }
-        cout << "\n";
-
+        cout<<"\n";
     }
+    for (int i = n-1; i >= 0; i--){
+        for (int j = i; j >= 0; j--){
+            cout <<"*";
+        }
+        for (int j = 0; j < (2*(n-i)-2); j++){
+            cout <<" ";
+        }
+        for (int j = i; j >= 0; j--){
+            cout <<"*";
+        }
+        cout <<"\n";
+    }
+    
+    
     return 0;
 }
