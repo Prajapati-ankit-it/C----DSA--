58af78a7b63f54ee34d472ef34c1a958c8c7a131
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2025-12-30 13:02:31 +0530

    Add swapMinMax program to swap the maximum and minimum elements in an array

---
+++ Array/swapMinMax.cpp
@@ -0,0 +1,41 @@
+//WAF to swap the max & min number of an array
+
+#include <iostream>
+#include <climits>
+#include <algorithm>
+using namespace std;
+int SIZE = 0;
+
+void maxMinSwapArray(int arr[]){
+    int smallest = INT_MAX, largest = INT_MIN, smallest_I = 0, largest_I = 0;
+    for (int i = 0; i < SIZE; i++){
+        if (arr[i] < smallest){
+            smallest = arr[i];
+            smallest_I = i;
+        }
+        if (arr[i] < largest){
+            largest = arr[i];
+            largest_I = i;
+        }
+    }
+    swap(arr[smallest_I], arr[largest_I]);
+}
+
+int main(){
+    int target;
+    cout <<"Enter Array Size: ";
+    cin >> SIZE;
+    cout << "\n";
+
+    int arr[SIZE];   
+    cout << "Enter Age: ";
+    for (int i = 0; i < SIZE; i++){
+        cin >> arr[i];
+    }
+    maxMinSwapArray(arr);
+
+        for (int i = 0; i < SIZE; i++){
+        cout << arr[i] <<"\n";
+    }
+    
+}
