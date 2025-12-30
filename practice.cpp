7c7a889bbbfe3c16cad00b1329416af21f895ad1
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2025-12-31 00:35:10 +0530

    Add intersectionArray program to print intersection of two arrays

--- practice.cpp
+++ practice.cpp
@@ -1,44 +1,60 @@
-//WAF to swap the max & min number of an array
+//WAF to print intersection of 2 array
 
 #include <iostream>
-#include <climits>
-#include <algorithm>
 using namespace std;
-int SIZE = 0;
 
-void maxMinSwapArray(int arr[]){
-    int smallest = INT_MAX, largest = INT_MIN, smallest_I = 0, largest_I = 0;
-    for (int i = 0; i < SIZE; i++){
-        largest = max(arr[i],largest);
-        smallest = min(arr[i],smallest);
-    }
+void intersectionArray(int arr1[], int arr2[], int SIZE1, int SIZE2){
 
-    for (int i = 0; i < SIZE; i++){
-        if(smallest == arr[i]){
-            smallest_I = i;
-        }
-        if (largest == arr[i]){
-            largest_I = i;
+    for(int i = 0; i < SIZE1; i++){
+
+       for (int j = 0; j < SIZE2; j++){
+
+            if (arr1[i] == arr2[j]){
+
+                int count = 0;
+                for (int k = 0; k < i; k++){
+
+                    if (arr1[i] == arr1[k]){
+                        count++;
+                    }
+                }
+                for (int k = 0; k < j; k++){
+
+                    if (arr2[j] == arr2[k]){
+                        count++;
+                    }
+                }
+                if (count == 0){
+                    cout << "Intersection of given Array: "<<arr1[i]<<"\n";
+                    break; 
+                }
+            }
         }
     }
-    swap(arr[smallest_I], arr[largest_I]);
 }
 
 int main(){
-    int target;
-    cout <<"Enter Array Size: ";
-    cin >> SIZE;
+    int SIZE1 = 0, SIZE2 = 0;
+    cout <<"Enter Array1 Size: ";
+    cin >> SIZE1;
     cout << "\n";
 
-    int arr[SIZE];   
-    cout << "Enter Age: ";
-    for (int i = 0; i < SIZE; i++){
-        cin >> arr[i];
+    int arr1[SIZE1];   
+    cout << "Enter Array1: ";
+    for (int i = 0; i < SIZE1; i++){
+        cin >> arr1[i];
     }
-    maxMinSwapArray(arr);
 
-        for (int i = 0; i < SIZE; i++){
-        cout << arr[i] <<"\n";
+    cout <<"Enter Array2 Size: ";
+    cin >> SIZE2;
+    cout << "\n";
+    int arr2[SIZE2];
+
+    cout << "Enter Array2: ";
+    for (int i = 0; i < SIZE2; i++){
+        cin >> arr2[i];
     }
+    
+    intersectionArray(arr1,arr2,SIZE1,SIZE2);
     
 }
