0d55bb86861358300af4c8adfbd9b1397b8e2c70
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2025-12-31 16:30:00 +0530

    Refactor practice.cpp to implement linear search function and update main for user input

--- practice.cpp
+++ practice.cpp
@@ -1,14 +1,31 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
 
-int main(){
-    vector<int> vec = {1,2,3};
-    vec.push_back(4);
-    vec.push_back(5);
-    vec.push_back(6);
-    vec.push_back(7);
 
-    cout<<"capacity: "<<vec.capacity();
-    return 0;
+int linearSearch(vector<int>& arr, int target){    
+    // int count = 0;    
+    // for(int i : arr){
+    //     count++;
+    //     if (i == target){
+    //         return count-1;
+    //     }
+    // }
+    // return -1;
+    int length = arr.size();
+    for (int i = 0; i < length; i++){
+        if (arr[i] == target){
+            return i;
+        }
+    }
+    return -1;
+    
+}
+
+int main (){
+    vector<int> arr = {1,2,3};
+    int target = 0;
+    cout<<"Enter what you want to find: ";
+    cin >> target;
+    cout<< linearSearch(arr, target); 
 }
