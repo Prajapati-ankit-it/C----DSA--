8324026a62216a853623a4e82b8dcd2d52b22e7a
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2026-01-06 16:27:15 +0530

    Add majorityElementMoorseV function for O(1) space complexity majority element detection

--- Array/vector/majorityElementB.cpp
+++ Array/vector/majorityElementB.cpp
@@ -41,8 +41,38 @@ int majorityElement(vector<int> &arr){//O(nlogn)
     return -1;
 }
 
+int majorityElementMoorseV(vector<int> &arr){//O(1)
+
+    int n = arr.size();
+    
+    int frequency = 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++){
+        if (frequency == 0){
+            ans = arr[i];
+        }
+        if ( ans == arr[i]){
+            frequency++;
+        }else{
+            frequency--;
+        }
+    }
+    int count = 0;
+    for(int n : arr){
+        if (n == ans){
+            count++;
+        }
+    }
+    if (count > n/2){
+        return ans;
+    }else{
+        return -1;
+    }
+    return ans;
+}
+
 int main (){
-    vector<int> arr = {1,1,2,2,2};
-    cout << majorityElement(arr);
+    vector<int> arr = {1,2,3,4};
+    cout << majorityElementMoorseV(arr);
 
 }
