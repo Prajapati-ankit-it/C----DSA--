d152f8ea8cb17f33b63630fe595c74896f068e4f
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2026-01-10 21:26:05 +0530

    Refactor productExceptSelf function to improve efficiency by eliminating nested loops and using a single pass for calculations

--- practice.cpp
+++ practice.cpp
@@ -1,22 +1,25 @@
 #include<iostream>
 #include<vector>
-#include <algorithm>
+#include<algorithm>
 using namespace std;
 
 vector<int> productExceptSelf(vector<int> &arr){
 
     int n = arr.size();
-    vector<int> ans;
-    for(int i = 0; i < n; i++){
-        int product = 1;
-        for (int j = 0; j < n; j++){
-            if (i != j){
-                product *= arr[j];
-            }
-        }
-        ans.push_back(product);
+    vector<int> ans(n,1);
+    // vector<int> sufix(n,1);
+    int sufix = 1;
+    for(int i = 1; i < n; i++){
+        ans[i] = ans[i-1] * arr[i-1];
     }
-
+    for (int i = n-2; i >= 0; i--){
+        sufix *= arr[i+1]; 
+        ans[i] *= sufix;
+    }
+    // for (int i = 0; i < n; i++){
+    //     ans[i] = sufix[i] * ans[i];
+    // }
+    
     return ans;
 }
 
