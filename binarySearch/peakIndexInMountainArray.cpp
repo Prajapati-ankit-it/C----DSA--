cf1b2d9c175a330be3807f4a6039f89fc9e6d11b
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2026-01-13 16:23:29 +0530

    Add peakIndexInMountainArray function to find the peak index in a mountain array

---
+++ binarySearch/peakIndexInMountainArray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int peakIndexInMountainArray(vector<int>& arr) {
+        int st = 1, end = arr.size()-2;
+        while(st <= end){
+            int mid = st + (end - st)/2;
+            if(arr[mid-1] < arr[mid] && arr[mid+1] < arr[mid]){
+                return mid;
+            }
+            if(arr[mid] > arr[mid-1]){
+                st = mid+1;
+            }else if(arr[mid] < arr[mid-1]){
+                end = mid-1;
+            }
+        }
+    }
+
+int main (){
+    vector<int> arr = {1,2,3,4,5,4,3,2,1,};
+    int result = peakIndexInMountainArray(arr);
+    cout << result;
+
+    return 0;
+
+}
