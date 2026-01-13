413da373cd2efed931dfde06ea0cecb7346ac743
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2026-01-13 16:22:39 +0530

    Refactor peakIndexInMountainArray function and remove unused productExceptSelf function

--- practice.cpp
+++ practice.cpp
@@ -3,34 +3,25 @@
 #include<algorithm>
 using namespace std;
 
-vector<int> productExceptSelf(vector<int> &arr){
-
-    int n = arr.size();
-    vector<int> ans(n,1);
-    // vector<int> sufix(n,1);
-    int sufix = 1;
-    for(int i = 1; i < n; i++){
-        ans[i] = ans[i-1] * arr[i-1];
+int peakIndexInMountainArray(vector<int>& arr) {
+        int st = 1, end = arr.size()-2;
+        while(st <= end){
+            int mid = st + (end - st)/2;
+            if(arr[mid-1] < arr[mid] && arr[mid+1] < arr[mid]){
+                return mid;
+            }
+            if(arr[mid] > arr[mid-1]){
+                st = mid+1;
+            }else if(arr[mid] < arr[mid-1]){
+                end = mid-1;
+            }
+        }
     }
-    for (int i = n-2; i >= 0; i--){
-        sufix *= arr[i+1]; 
-        ans[i] *= sufix;
-    }
-    // for (int i = 0; i < n; i++){
-    //     ans[i] = sufix[i] * ans[i];
-    // }
-    
-    return ans;
-}
 
 int main (){
-    vector<int> arr = {1,2,3,4};
-    
-    vector<int> result = productExceptSelf(arr);
-
-    for (int x : result) {   
-        cout << x << " ";
-    }
+    vector<int> arr = {1,10,2,3,4,5,6,7,8,9,};
+    int result = peakIndexInMountainArray(arr);
+    cout << result;
 
     return 0;
 
