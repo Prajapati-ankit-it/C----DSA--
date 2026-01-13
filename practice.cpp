61c32af191fdd2a50579bf2684b4c933664b64f9
Prajapati-ankit <105482209+Prajapati-ankit-it@users.noreply.github.com>
2026-01-13 16:23:22 +0530

    Fix input array in main function for peakIndexInMountainArray test case

--- practice.cpp
+++ practice.cpp
@@ -19,7 +19,7 @@ int peakIndexInMountainArray(vector<int>& arr) {
     }
 
 int main (){
-    vector<int> arr = {1,10,2,3,4,5,6,7,8,9,};
+    vector<int> arr = {1,2,3,4,5,4,3,2,1,};
     int result = peakIndexInMountainArray(arr);
     cout << result;
 
